445f3780429b19536b33de895e1b3a93dfeb3769
Vitor Dorea <vitordanniel@hotmail.com>
2026-02-28 08:34:11 -0300

    Update main.py
    Adicionada Docstrings nas funções e validação no ano de nascimento

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from datetime import date
 
 def titulos(txt):
+    """
+    -> Padronizador de títulos
+    :param txt: o texto do título
+    """
+
     print('=' * 30)
     print(txt.center(30))
     print('=' * 30)
 
+
 def menu(): 
+    """
+    -> Menu do programa
+    """
 
     titulos('CADASTRADOR DE USUÁRIOS')
+
     print('1 - Casdatrar usuários')
     print('2 - Listar usuários')
     print('3 - Atualizar usuários')
@@ -15,26 +25,50 @@ def menu():
     print('0 - Encerrar programa')
     print('=' * 30)
 
+
 def lerInt(txt):
+    """
+    -> Retorna um número inteiro
+    :param txt: texto do input
+    """
+
     while True:
         try:
             return int(input(txt))
         except ValueError:
             print('\033[31mDigite um valor válido! \033[m')
 
+
 def cadastrarUsuarios(usuarios):
+    """
+    Registra usuários e guarda na lista de usuários
+    :param usuarios: lista de usuários
+    """
 
     titulos('Cadastrar Usuários')
 
     usuario = {}
     usuario['ID'] = len(usuarios) + 1 
     usuario['Nome'] = input('Nome: ')
-    nasc = lerInt('Ano de Nascimento: ')
-    usuario['Idade'] = date.today().year - nasc
+
+    while True:
+        nasc = lerInt('Ano de Nascimento: ')
+        if 1900 < nasc < date.today().year:
+            break
+        else:
+            print('\033[31mDigite um ano válido!\033[m')
+
+    usuario['Idade'] =  date.today().year - nasc
     usuarios.append(usuario)
 
 def listarUsuarios(usuarios):
+    """
+    Lista todos usuários cadastrados
+    :param usuarios: lista de usuários
+    """
+
     titulos('Usuários cadastrados')
+
     for usuario in usuarios:
         print(f'ID: {usuario["ID"]} | Nome: {usuario["Nome"]} | Idade: {usuario["Idade"]} anos')
 
@@ -55,8 +89,11 @@ while True:
     elif opcao == 2:
         listarUsuarios(usuarios)
     elif opcao == 3:
+        print('Funcao em desenvolvimento')
         print('Atualizar')
     elif opcao == 4:
+        print('Funcao em desenvolvimento')
         print('Deletar')
     elif opcao == 0:
         print('Encerrando programa... ')
+        break
