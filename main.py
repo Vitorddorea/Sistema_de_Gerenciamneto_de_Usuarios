166fe290183f6f36a366d79dc0a51f8bea093374
Vitor Dorea <vitordanniel@hotmail.com>
2026-03-02 22:25:48 -0300

    Update main.py
    Adicionada as funções deletar e atualizar, adicionado também Docstrings

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def titulos(txt):
     print(txt.center(30))
     print('=' * 30)
 
-
 def menu(): 
     """
     -> Menu do programa
@@ -18,13 +17,12 @@ def menu():
 
     titulos('CADASTRADOR DE USUÁRIOS')
 
-    print('1 - Casdatrar usuários')
+    print('1 - Cadastrar usuários')
     print('2 - Listar usuários')
     print('3 - Atualizar usuários')
     print('4 - Deletar usuários')
     print('0 - Encerrar programa')
     print('=' * 30)
-
 
 def lerInt(txt):
     """
@@ -38,6 +36,31 @@ def lerInt(txt):
         except ValueError:
             print('\033[31mDigite um valor válido! \033[m')
 
+def criarID(usuarios):
+    """
+    Cria um ID único
+    :param usuarios: lista de usuários
+    """
+    
+    if not usuarios:
+        return 1
+    else:
+        ultimo_id = max(usuario['ID'] for usuario in usuarios)
+        return ultimo_id + 1
+
+def lerNasc():
+    """
+    Retorna um ano de nascimento válido
+    """
+
+    while True:
+        nasc = lerInt('Ano de Nascimento: ')
+        if 1900 < nasc < date.today().year:
+            break
+        else:
+            print('\033[31mDigite um ano válido!\033[m')
+
+    return nasc
 
 def cadastrarUsuarios(usuarios):
     """
@@ -48,18 +71,16 @@ def cadastrarUsuarios(usuarios):
     titulos('Cadastrar Usuários')
 
     usuario = {}
-    usuario['ID'] = len(usuarios) + 1 
+
+    usuario['ID'] = criarID(usuarios)
     usuario['Nome'] = input('Nome: ')
 
-    while True:
-        nasc = lerInt('Ano de Nascimento: ')
-        if 1900 < nasc < date.today().year:
-            break
-        else:
-            print('\033[31mDigite um ano válido!\033[m')
+    nasc = lerNasc()
 
     usuario['Idade'] =  date.today().year - nasc
     usuarios.append(usuario)
+
+    print('Usuário cadastrado!')
 
 def listarUsuarios(usuarios):
     """
@@ -69,31 +90,78 @@ def listarUsuarios(usuarios):
 
     titulos('Usuários cadastrados')
 
+    if not usuarios:
+        print('Nenhum usuários cadastrado!')
+    else:
+        for usuario in usuarios:
+            print(f'ID: {usuario["ID"]} | Nome: {usuario["Nome"]} | Idade: {usuario["Idade"]} anos')
+
+def atualizarUsuario(usuarios):
+    """
+    Atualiza um usuário com base no seu ID
+    :param usuarios: lista de usuários
+    """
+
+    titulos('Atualizar usuários')
+
+    encontrado = False
+
     for usuario in usuarios:
-        print(f'ID: {usuario["ID"]} | Nome: {usuario["Nome"]} | Idade: {usuario["Idade"]} anos')
+        if usuario['ID'] == opcao:
+            usuario['Nome'] = input('Nome: ')
+            nasc = lerNasc()
+            usuario['Idade'] = date.today().year - nasc
+            encontrado = True
+            break
 
+    if encontrado:
+        print(f'Usuário do ID {opcao} atualizado!')
+    else:
+        print('Usuário não encontrado.')
 
-#def atualizarUsuario(usuarios):
+def deletarUsuario(usuarios):
+    """
+    Deleta um usuário com base no seu ID
+    :param usuarios: lista de usuários
+    """
+    
+    titulos('Deletar usuário')
 
-#def deletarUsuario():
+    if not usuarios:
+        print('Nenhum usuários cadastrado!')
+    else:
+        listarUsuarios(usuarios)
 
+        opcao = lerInt('Qual usuário você deseja deletar? ID: ')
+
+        for usuario in usuarios:
+            if usuario['ID'] == opcao:
+                usuarios.remove(usuario)
+
+        print('Usuário do ID {opcao} deletado!')
+    
 usuarios = []
 
 while True:
     menu()
 
-    opcao = lerInt('Escolha uma opção: ')
+    while True:
+        opcao = lerInt('Escolha uma opção: ')
+        if opcao not in (1, 2, 3, 4, 0):
+            print('\033[31m Digite uma opção válida! \033[m')
+        else:
+            break
 
     if opcao == 1: 
         cadastrarUsuarios(usuarios)
     elif opcao == 2:
         listarUsuarios(usuarios)
     elif opcao == 3:
-        print('Funcao em desenvolvimento')
-        print('Atualizar')
+        atualizarUsuario(usuarios)
     elif opcao == 4:
-        print('Funcao em desenvolvimento')
-        print('Deletar')
+        deletarUsuario(usuarios)
     elif opcao == 0:
         print('Encerrando programa... ')
         break
+        
+        
